adfcf489961dafd839dd9fe8643c72c4b7f7f8ca
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-27 21:15:40 +0000

    add: beat kick playback

--- src/components/Beat.jsx
+++ src/components/Beat.jsx
@@ -1,10 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { PlayHeadContext } from "../App";
+
+import kick from "../assets/audio/kick.wav";
+import { playSound } from "../utilities/audio";
 
 export default function Beat({ beat }) {
   const playHeadPos = useContext(PlayHeadContext);
-
   const displayPlayHead = playHeadPos === beat.beatNum;
+
+  useEffect(() => {
+    if (displayPlayHead && beat.hit) {
+      const audioContext = new AudioContext();
+      playSound(audioContext, kick, 0);
+      return () => setTimeout(() => audioContext.close(), 2000);
+    }
+  }, [displayPlayHead]);
 
   return (
     <label
