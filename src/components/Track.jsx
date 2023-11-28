c02071692e078d141b1714ab50be584ccbd4ab17
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-28 12:50:49 +0000

    remove: console log

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -2,7 +2,6 @@ import Beat from "./Beat";
 
 export default function Track({ track }) {
   const { beats, trackId, trackName } = track;
-  console.log("track", track);
   return (
     <div className="flex gap-2">
       {trackName}
