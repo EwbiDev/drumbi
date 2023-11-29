76e09c9cc0698033e9f30012610279bd57a31f3c
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-11-29 14:18:31 +0000

    add: scheduleNote && loadBuffer

--- src/utilities/scheduler.js
+++ src/utilities/scheduler.js
@@ -1,9 +1,28 @@
 import { getAudioCtx } from "./audioContext";
+import kickPath from "@assets/audio/kick.wav";
 
 const audioCtx = getAudioCtx();
+let kickBuffer = null;
+
+async function loadBuffer() {
+  if (!kickBuffer) {
+    const response = await fetch(kickPath);
+    const audioData = await response.arrayBuffer();
+    kickBuffer = await audioCtx.decodeAudioData(audioData);
+  }
+}
 
 function scheduler() {
-  console.log("audioCtx", audioCtx.currentTime);
+  loadBuffer();
+  scheduleNote();
+}
+
+async function scheduleNote() {
+  const source = audioCtx.createBufferSource();
+  source.buffer = kickBuffer;
+  source.connect(audioCtx.destination);
+
+  source.start();
 }
 
 export { scheduler };
