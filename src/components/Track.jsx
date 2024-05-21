8844975a25afcb3cade6724f43d43e6d618476ff
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-05-21 14:51:21 +0100

    fix: improve track label spacing

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -4,7 +4,7 @@ export default function Track({ track }) {
   const { beats, trackId, trackName } = track;
   return (
     <div className="flex items-center gap-2 text-white">
-      <div className="w-12 text-right">{trackName}</div>
+      <div className=" w-24 text-right">{trackName}</div>
       {beats.map((beat) => (
         <Beat beat={beat} trackId={trackId} defaultFile={track.defaultFile} />
       ))}
